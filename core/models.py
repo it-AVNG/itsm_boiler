8ff61acfe7aafd25e3b1d3d17b2f6721b9c13474
banzdec <it.avng@gmail.com>
2025-07-07 20:44:06 +0700

    feat(customUserManager): implement checks for valueError when failed to createuser and superuser

--- core/models.py
+++ core/models.py
@@ -6,35 +6,51 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 from django.utils import timezone
+
+from django.core.validators import EmailValidator
+from django.core.exceptions import ValidationError
 # Create your models here.
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         """
         Creates and saves a User with the given email
         """
         if not email:
             raise ValueError("Users must have an email address")
 
-        user = self.model(
-            email=self.normalize_email(email),
-        )
+        email_validator = EmailValidator()
+        try:
+            email_validator(email)
+        except ValidationError:
+            raise ValueError("Enter a valid email")
+            
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.is_staff = False
+        user.is_superuser = False
+        user.is_admin = False
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, email, password=None, **extra_fields):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
         """
-        user = self.create_user(
-            email,
-            password=password,
-        )
+        if (
+            extra_fields.get("is_staff") == False
+            or extra_fields.get("is_superuser") == False
+            or extra_fields.get("is_admin") == False
+        ):
+            raise ValueError("Super user is staff member")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
         user.is_staff = True
         user.is_superuser = True
+        user.is_admin = True
         user.save(using=self._db)
         return user
 
